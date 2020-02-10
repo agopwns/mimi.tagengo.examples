2bb8aa527965551176307fac3c75c46a11c275ab
SeongJun Ha <agopwns@naver.com>
2020-02-10 17:48:27 +0900

    add 대화 번역 UI

---
+++ src/Android/TagengoExampleApp/app/src/main/java/jp/fairydevices/mimi/example/TranslationConversation.java
@@ -0,0 +1,13 @@
+package jp.fairydevices.mimi.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranslationConversation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translation_conversation);
+    }
+}
