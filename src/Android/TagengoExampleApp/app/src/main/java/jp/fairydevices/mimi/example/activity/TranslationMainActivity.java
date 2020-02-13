1854e52efe97718d809c22f981fe87888c5f37bd
SeongJun Ha <agopwns@naver.com>
2020-02-13 16:23:02 +0900

    인식 결과 초기화 버튼 추가

--- src/Android/TagengoExampleApp/app/src/main/java/jp/fairydevices/mimi/example/activity/TranslationMainActivity.java
+++ src/Android/TagengoExampleApp/app/src/main/java/jp/fairydevices/mimi/example/activity/TranslationMainActivity.java
@@ -4,6 +4,8 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -17,10 +19,13 @@ import jp.fairydevices.mimi.example.R;
 
 public class TranslationMainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton srButton;
-    private ImageButton mtButton;
-    private EditText srOutput;
-    private EditText mtOutput;
+    private ImageButton srButton; // 음성 인식
+    private ImageButton mtButton; // 기계 번역
+    private ImageButton ssButton; // 음성 합성
+    private ImageButton clearButton; // 인식 결과 초기화
+
+    private EditText srOutput; // 인식 결과 - 번역할 문장
+    private EditText mtOutput; // 번역 결과
 
     private boolean isRecording = false;
     private PrismClient prismClient = null;
@@ -30,22 +35,51 @@ public class TranslationMainActivity extends AppCompatActivity implements View.O
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_translation_main);
 
-        checkPermission(this); // 번역 메인 화면에서 딱 한번만 검사하면 됨
+        checkPermission(this); // 번역 메인 화면에서 한 번만 검사하면 됨
         initView(); // 레이아웃 뷰 초기화
 
         prismClient = new PrismClient(srOutput, mtOutput);
         prismClient.updateToken(); // 액세스 토큰 취득
+
+        // 인식 결과 이벤트
+        srOutput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // 입력되는 텍스트에 변화가 있을 때
+
+                if(srOutput.getText().length() == 0){
+                    // x 이미지 버튼 invisible
+                    clearButton.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable arg0) {
+                // 입력이 끝났을 때
+                // x 이미지 버튼 visible
+                clearButton.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // 입력하기 전에
+            }
+        });
     }
 
     private void initView() {
         srButton = findViewById(R.id.srButton);
         mtButton = findViewById(R.id.mtButton);
-//        Button ssButton = findViewById(R.id.ssButton);
+        ssButton = findViewById(R.id.ssButton);
+        clearButton = findViewById(R.id.clearButton);
         srOutput = findViewById(R.id.srOutputText);
         mtOutput = findViewById(R.id.mtOutputText);
         srButton.setOnClickListener(this);
         mtButton.setOnClickListener(this);
-//        ssButton.setOnClickListener(this);
+        ssButton.setOnClickListener(this);
+        clearButton.setOnClickListener(this);
     }
 
     private void checkPermission(Activity activity) {
@@ -80,11 +114,21 @@ public class TranslationMainActivity extends AppCompatActivity implements View.O
                 break;
             case R.id.mtButton: // 기계번역
                 prismClient.MT();
+
+                // 번역 후 음성 합성 버튼 나오게
+
+                ssButton.setVisibility(View.VISIBLE);
                 break;
-//            case R.id.ssButton: // 음성합성
-//                prismClient.SS();
-//                break;
-            default:
-        }
+            case R.id.ssButton: // 음성합성
+                // TODO : 합성된 음성 크기가 최소인 부분 고쳐야 함.
+                prismClient.SS();
+                break;
+            case R.id.clearButton: // 인식 텍스트 초기화
+                srOutput.setText("");
+                clearButton.setVisibility(View.INVISIBLE);
+                break;
+            default:        }
     }
+
+
 }
